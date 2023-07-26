1e0c92b06bb79f2c38195bcf0e8a22c278121cb4
Payal Kandlur <payalkandlur@Payals-MacBook-Air.local>
2023-07-26 14:56:59 +0530

    first commit

---
+++ Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var myset: Set = [1,2,3,4,5]
+myset.insert(5)
+print(myset)
+//unordered, plus cannot ahve duplicate values
+
+var myArray: Array = [1,2,3,4,5]
+myArray.append(5)
+print(myArray)
+//ordered, can ahve duplicate values
